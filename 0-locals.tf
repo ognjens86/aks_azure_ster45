955706e12754b6d20e77f0bb39685e2a31476113
ognjens86 <61202292+ognjens86@users.noreply.github.com>
2025-02-18 18:03:01 +0100

    Update 0-locals.tf

--- 0-locals.tf
+++ 0-locals.tf
@@ -1,7 +1,7 @@
 locals {
   env                 = "dev"
   region              = "canadacentral"
-  resource_group_name = "zzz-aks-01"
+  resource_group_name = "zzz-aks-02"
   eks_name            = "zzz-aks"
   eks_version         = "1.31"
 }
