764a7c373593a1145af004427f53621aba1ef6ba
ognjens86 <61202292+ognjens86@users.noreply.github.com>
2025-02-19 14:35:42 +0100

    Update 9-workload-identity.tf

--- 9-workload-identity.tf
+++ 9-workload-identity.tf
@@ -1,16 +1,17 @@
-resource "azurerm_user_assigned_identity" "dev_test" {
-  name                = "dev-test"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+# Fetch the Service Principal
+data "azurerm_service_principal" "ognjen" {
+  display_name = "ognjen-service-principal"
 }
 
+# Create Federated Identity Credential
 resource "azurerm_federated_identity_credential" "dev_test" {
   name                = "dev-test"
   resource_group_name = local.resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = azurerm_kubernetes_cluster.this.oidc_issuer_url
-  parent_id           = azurerm_user_assigned_identity.dev_test.id
+  parent_id           = data.azurerm_service_principal.ognjen.id  # Use the service principal ID
   subject             = "system:serviceaccount:dev:my-account"
 
   depends_on = [azurerm_kubernetes_cluster.this]
 }
+
